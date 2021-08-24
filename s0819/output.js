c2a8b83505e67ceeefd3e5cfb79f55e33900b7a0
taeyeon051 <y2010112@y-y.hs.kr>
2021-08-24 15:37:38 +0900

    0819 수업

---
+++ s0819/output.js
@@ -0,0 +1,13 @@
+// html 요소로 출력 작성
+document.querySelector("#out").innerHTML
+    = "<pre>// html요소로 출력 작성\ndocument.querySelector(\"#demo\").innerHTML</pre>";
+
+// document 출력으로 작성
+document.write("<pre>// document 출력으로 작성\ndocument.write()</pre>");
+
+// window 객체 경고창을 사용하여 출력
+window.alert("// window 객체 경고창을 사용하여 출력\nwindow.alert()");
+
+// 브라우저 콘솔을 사용하여 출력
+let logWrite = "// 브라우저 콘솔을 사용하여 출력 => console.log()";
+console.log(logWrite);
