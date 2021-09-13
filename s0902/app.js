aca98bf02af3fddeb9226ba23207cd6392e6e374
taeyeon051 <y2010112@y-y.hs.kr>
2021-09-13 23:28:58 +0900

    0902 수업

--- s0902/app.js
+++ s0902/app.js
@@ -1,54 +1,42 @@
-function ex2_a() {
-    document.querySelector("#exQuery").innerText = "a. 빈 배열을 생성하고, 배열에 값을 추가, 출력해 봅시다.";
-
-    // 빈 배열 생성
-    const arrNum = [];
-
-    // 지역변수 num1
-    let num1 = prompt("숫자를 입력하세요.", "0 ~ 99");
-    arrNum.push(num1);
-
-    // 출력
-    let exAnswer = document.querySelector("#exAnswer");
-    exAnswer.innerText = `입력받은 수의 값은 : \n ${arrNum}입니다.`;
+// 윈도우 객체 load
+function winLoad(e) {
+    alert(`${e.currentTarget} 객체가 ${e.type}되었습니다.`);
 }
 
-// 전역변수 생성
-let count = 3;
+window.onload = winLoad;
 
-function ex2_b() {
-    let exAnswer = document.querySelector("#exAnswer");
-    document.querySelector("#exQuery").innerText = "b. 빈 배열 생성, count 변수만큼 입력, count 변수만큼 출력";
+// H1에 이벤트리스너 추가 (익명이너 함수)
+document.querySelector("h1").addEventListener("click", () => {
+    alert(`${this}를 클릭했습니다.`);
+    alert(`${this.textContent}를 클릭했습니다.`);
+});
 
-    // 빈 배열 생성
-    const arrNum = new Array();
-
-    // count 만큼 입력
-    for (let i = 0; i < count; i++) arrNum.push(prompt(i + "번째 숫자를 입력하세요.", "0 ~ 99"));
-
-    // 입력된 배열의 갯수만큼 출력
-    exAnswer.innerText = "";
-    for (i in arrNum) exAnswer.innerText += arrNum[i] + "\n";
+// P에 이벤트리스너 추가 (call 외부함수)
+function dbClick(e) {
+    alert(`${e.currentTarget.textContent}를 더블클릭했습니다.`);
+    alert(`${e.currentTarget.tagName}태그를 ${e.type}했습니다.`);
 }
 
-function addTagP(v) {
-    let p = document.createElement("p");
-    let txt = document.createTextNode(v);
-    p.appendChild(txt);
-    p.classList.add("my-2", "bg-info");
+document.querySelector("#dbClk").addEventListener("dblclick", dbClick);
 
-    let exAnswer = document.querySelector("#exAnswer");
-    exAnswer.appendChild(p);
+// Button에 이벤트리스너 추가
+function over() {
+    alert("첫번째버튼만 오버됩니다.");
 }
 
-function ex2_c() {
-    document.querySelector("#exQuery").innerText = "c. 빈 배열 생성, count 만큼 입력함수(), p 태그로 출력함수()";
-    // 빈 배열 생성
-    const arrNum = new Array();
+document.querySelector("button").addEventListener("mouseover", over);
 
-    // count 만큼 입력
-    for (let i = 0; i < count; i++) arrNum.push(prompt(i + "번째 숫자를 입력하세요.", "0 ~ 99"));
+// Button들에 배경스타일 지정
+let btns = document.querySelectorAll("button");
+btns.forEach(btn => {
+    btn.style.background = "aqua";
+});
 
-    // 입력된 배열의 갯수만큼 출력
-    for (let i in arrNum) addTagP(arrNum[i]);
+// Button들에 이벤트리스너 추가
+function dbClkS(e) {
+    alert(`${e.currentTarget.textContent}이 ${e.type}되었습니다.`);
 }
+
+btns.forEach(btn => {
+    btn.addEventListener("dblclick", dbClkS);
+});
